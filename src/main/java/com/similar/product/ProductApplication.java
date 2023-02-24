d6d23e66c14e3d2ff582d1dc8e033aac2e7dc48b
dsgarcia <davidsamuelgarciasanchez@gmail.com>
2023-02-24 03:45:23 -0300

    First Commit

---
+++ src/main/java/com/similar/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.similar.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
